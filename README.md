ad2e4161ea9e16169754ff0758576923956e3b38
zkfy <lib4net@163.com>
2019-06-24 17:19:14 +0800

    添加使用说明

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # qtask
-管理任务信息，定时将任务放入消息队列，直到任务关闭
+定时将任务放入消息队列，并提供延时，处理中，完结等控制任务处理过程。
+
+
+示例:
+
+* １. 创建任务
+
+```go
+//任务处理逻辑
+
+//创建超时执行任务
+qtask.Create(c,"订单绑定任务",map[string]interface{}{
+    "order_no":"8973097380045"
+},3600,"GCR:ORDER:BIND")
+
+```
+
+
+* 2. 编写MQC服务，该服务处理 `GCR:ORDER:BIND`消息队列数据
+
+```go
+
+func OrderBind(ctx *context.Context) (r interface{}) {
+    //检查输入参数...
+    
+    //将任务修改为正在处理中
+    qtask.Processing(ctx,ctx.Request.GetInt64("task_id"))
+
+
+    //处理其它逻辑...
+
+
+    //成功处理，结束任务
+    qtask.Finish(ctx,ctx.Request.GetInt64("task_id"))
+}
+
+```
+
+* 3. 配置定时任务
+
+a. 注册服务
+
+```go
+app.Cron("/task/scan",qtask.Scan) //定时扫描任务
+app.Cron("/task/clear",qtask.Clear(7)) //定时清理任务，删除７天的任务数据
+
+
+```
+
+b. 配置定时执行时间
+```sh
+app.Conf.CRON.SetSubConf("task", `{
+		"tasks": [
+			{
+				"cron": "@every 10s",
+				"service": "/task/scan"
+			},
+			{
+				"cron": "@every 10s",
+				"service": "/task/clear"
+			}
+        ]
+    }`)
+```
+
+
--- task.go
+++ task.go
@@ -1,33 +1,68 @@
 package qtask
 
 import (
+	"github.com/micro-plat/hydra/component"
+	"github.com/micro-plat/hydra/context"
 	"github.com/micro-plat/lib4go/db"
 )
 
 //Create 创建任务,返回任务编号和错误信息
+//c:*context.Context,component.IContainer,db.IDB
 //name:任务名称
 //input:任务关健参数，序列化为json后存储，一般只允许传入关键参数。系统会在此输入参数中增加一个参数"task_id",业务流程需使用"task_id"修改任务为处理中或完结任务
 //timeout:任务单次执行的超时时长，即下次执行时间距离上次执行时间的秒数，任务被放入消息队列、被标记为正在处理等都会自动调整下次执行时间
 //mq: 消息队列名称
-func Create(db db.IDB, name string, input map[string]interface{}, timeout int, mq string) (taskID int64, err error) {
-	return createTask(db, name, input, timeout, mq)
+func Create(c interface{}, name string, input map[string]interface{}, timeout int, mq string) (taskID int64, err error) {
+
+	db,err:=getDB(c)
+	if err!=nil{
+		return 0,err
+	}
+	return createTask(db, name, input, timeout, mq)	
+
 }
 
 //Delay 修改任务的下次执行时间，任务未完成时可调用此函数。
 //调用此函数将延后下次被放入消息队列的时间
-func Delay(db db.IDB, taskID int64) error {
+func Delay(c interface{},taskID int64) error {
+	db,err:=getDB(c)
+	if err!=nil{
+		return err
+	}
 	return delayTask(db, taskID)
 }
 
 //Processing 将任务修改为处理中。系统自动延时，并累加任务处理次数
 //任务被正式处理前调用此函数
 //调用后当下次执行时间小于当前时间后会重新放入消息队列进行处理
-func Processing(db db.IDB, taskID int64) error {
+func Processing(c interface{}, taskID int64) error {
+	db,err:=getDB(c)
+	if err!=nil{
+		return err
+	}
 	return processingTask(db, taskID)
 }
 
 //Finish 任务完成
 //任务终结，不再放入消息队列
-func Finish(db db.IDB, taskID int64) error {
+func Finish(c interface{},taskID int64) error {
+	db,err:=getDB(c)
+	if err!=nil{
+		return err
+	}
 	return finishTask(db, taskID)
+}
+
+func getDB(c interface{})(db db.IDB,error){
+	switch v := c.(type) {
+	case *context.Context:
+		return v.GetContainer().GetRegularDB(dbName),nil
+	case component.IContainer:
+		return v.GetRegularDB(dbName),nil
+	case db.IDB:
+		return v,nil
+	default:
+		return nil,fmt.Errorf("不支持的参数类型")
+	}
+
 }
