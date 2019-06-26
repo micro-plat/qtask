ef55a2cc33b860315277a71cc1d5d7d19d782807
zkfy <lib4net@163.com>
2019-06-26 17:03:07 +0800

    优化

--- service.go
+++ service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/micro-plat/hydra/conf"
+	"github.com/micro-plat/hydra/context"
 	"github.com/micro-plat/hydra/hydra"
 )
 
@@ -22,4 +23,38 @@ func Bind(app *hydra.MicroApp, scanSecond int, dayBefore int) {
 	ch := app.GetDynamicCron()
 	ch <- &conf.Task{Cron: fmt.Sprintf("@every %ds", scanSecond), Service: "/task/scan"}
 	ch <- &conf.Task{Cron: "@hourly", Service: "/task/clear"}
+}
+
+//Scan 扫描任务，定时从ＤＢ中扫描待处理任务并放入消息队列
+func Scan(ctx *context.Context) (r interface{}) {
+
+	ctx.Log.Info("---------------qtask:任务扫描----------------")
+	ctx.Response.SetPlain()
+	rows, err := queryTasks(ctx.GetContainer().GetRegularDB(dbName))
+	if err != nil {
+		return err
+	}
+	ctx.Log.Info("发送任务到消息队列")
+	queue := ctx.GetContainer().GetRegularQueue(queueName)
+	for _, row := range rows {
+		qName := row.GetString("queue_name")
+		content := row.GetString("content")
+		if err := queue.Push(qName, content); err != nil {
+			return fmt.Errorf("发送消息(%s)到消息队列(%s)失败:%v", content, qName, err)
+		}
+	}
+	ctx.Log.Infof("处理消息:%d条", rows.Len())
+	return "success"
+}
+
+//Clear 清理任务，删除指定时间以前的任务
+func Clear(day int) func(ctx *context.Context) (r interface{}) {
+	return func(ctx *context.Context) (r interface{}) {
+		ctx.Log.Infof("---------------qtask:清理%d天前的任务----------------", day)
+		err := clearTask(ctx.GetContainer().GetRegularDB(dbName), day)
+		if err != nil {
+			return err
+		}
+		return "success"
+	}
 }
