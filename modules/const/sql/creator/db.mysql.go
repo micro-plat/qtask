43548909641ffc2ed7e3232eb4fcd2894fd6fb74
luyue <luyue@100bm.cn>
2019-11-21 14:46:10 +0800

    修改引用

--- modules/const/sql/creator/db.mysql.go
+++ modules/const/sql/creator/db.mysql.go
@@ -3,11 +3,10 @@
 package creator
 
 import (
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/micro-plat/lib4go/db"
 )
 
 //CreateDB 自定义安装程序
-func CreateDB(xdb db.IDBExecuter) error {
+func CreateDB(xdb db.IDB) error {
 	return db.CreateDB(xdb, "src/github.com/micro-plat/qtask/modules/const/sql/mysql")
 }
--- modules/const/sql/creator/db.oracle.go
+++ modules/const/sql/creator/db.oracle.go
@@ -4,10 +4,9 @@ package creator
 
 import (
 	"github.com/micro-plat/lib4go/db"
-	_ "github.com/zkfy/go-oci8"
 )
 
 //自定义安装程序
-func CreateDB(xdb db.IDBExecuter) error {
+func CreateDB(xdb db.IDB) error {
 	return db.CreateDB(xdb, "src/github.com/micro-plat/qtask/modules/const/sql/oracle")
 }
