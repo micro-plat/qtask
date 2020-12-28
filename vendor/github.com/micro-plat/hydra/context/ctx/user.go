055f14b188b360e7ddc470033d42a86eedc333b2
zkfy <lib4net@163.com>
2020-12-28 16:09:01 +0800

    添加vendor

---
+++ vendor/github.com/micro-plat/hydra/context/ctx/user.go
@@ -0,0 +1,60 @@
+package ctx
+
+import (
+	"github.com/micro-plat/hydra/conf"
+	"github.com/micro-plat/hydra/context"
+	"github.com/micro-plat/hydra/global"
+)
+
+var _ context.IUser = &user{}
+
+type user struct {
+	gid string
+	conf.IMeta
+	ctx       context.IInnerContext
+	requestID string
+	auth      *Auth
+	jwtToken  interface{}
+}
+
+//NewUser 用户信息
+func NewUser(ctx context.IInnerContext, gid string, meta conf.IMeta) *user {
+	return &user{
+		ctx:   ctx,
+		gid:   gid,
+		auth:  &Auth{},
+		IMeta: meta,
+	}
+}
+
+//GetRequestID 获取请求编号
+func (c *user) GetRequestID() string {
+	if ids, ok := c.ctx.GetHeaders()[context.XRequestID]; ok {
+		global.RID.Add(ids[0])
+	}
+	return global.RID.GetXRequestID()
+}
+
+//GetGID 获取当前处理的goroutine id
+func (c *user) GetGID() string {
+	return c.gid
+}
+
+//GetUserName 获取用户名(basic认证启动后有效)
+func (c *user) GetUserName() string {
+	return c.GetString(context.UserName)
+}
+
+//GetClientIP 获取客户端IP地址
+func (c *user) GetClientIP() string {
+	ip := c.ctx.ClientIP()
+	if ip == "" || ip == "::1" || ip == "127.0.0.1" {
+		return global.LocalIP()
+	}
+	return ip
+}
+
+//Auth 用户认证信息
+func (c *user) Auth() context.IAuth {
+	return c.auth
+}
---
+++ vendor/github.com/micro-plat/hydra/creator/publish.input.go
@@ -0,0 +1,241 @@
+package creator
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/manifoldco/promptui"
+	vc "github.com/micro-plat/hydra/conf"
+	"github.com/micro-plat/lib4go/types"
+)
+
+//检查输入参数，并处理用户输入
+func checkAndInput(path string, v interface{}, input map[string]interface{}) error {
+
+	//处理参数类型
+	value := reflect.ValueOf(v)
+	vt := reflect.TypeOf(v)
+	if value.Kind() == reflect.Ptr {
+		value = value.Elem()
+		vt = vt.Elem()
+	}
+	if value.Kind() == reflect.Interface {
+		value = reflect.ValueOf(value.Interface())
+	}
+
+	//2. 根据类型分别处理
+	switch value.Kind() {
+	case reflect.Struct:
+		if !reflect.ValueOf(value).IsValid() || reflect.ValueOf(value).IsZero() {
+			return nil
+		}
+		return checkStruct(path, value, input)
+	case reflect.Map:
+		return checkMap(path, value, input)
+	default:
+		return checkString(path, value, input)
+	}
+}
+
+func checkString(path string, m reflect.Value, input map[string]interface{}) error {
+	return nil
+}
+
+func checkMap(path string, m reflect.Value, input map[string]interface{}) (err error) {
+	keys := m.MapKeys()
+	for _, key := range keys {
+		value := m.MapIndex(key)
+		skey := fmt.Sprint(key.Interface())
+		if !reflect.ValueOf(value).IsValid() || reflect.ValueOf(value).IsZero() {
+			continue
+		}
+		svalue := fmt.Sprint(value.Interface())
+		if !strings.HasPrefix(svalue, vc.ByInstall) && !strings.EqualFold(svalue, fmt.Sprint(vc.ByInstallI)) {
+			continue
+		}
+		v, ok := input[skey]
+		if !ok {
+			fname := getFullName(path, skey, "")
+			v, err = readFromCli(fname, skey, fname, "")
+			if err != nil {
+				return err
+			}
+		}
+		m.SetMapIndex(key, reflect.ValueOf(v))
+		continue
+
+	}
+	return nil
+}
+func checkStruct(path string, value reflect.Value, input map[string]interface{}) (err error) {
+	vt := reflect.TypeOf(value.Interface())
+	for i := 0; i < value.NumField(); i++ {
+		tfield := vt.Field(i)
+		vfield := value.Field(i)
+
+		//私有字段
+		if tfield.PkgPath != "" && !tfield.Anonymous { // unexported
+			continue
+		}
+		switch vfield.Kind() {
+		case reflect.Array, reflect.Slice: //目标字段为数组，将数据源为非数据转化为数组
+			if err := setSliceValue(path, vfield, tfield, input); err != nil {
+				return err
+			}
+		case reflect.Map:
+			if err := checkMap(path, vfield, input); err != nil {
+				return err
+			}
+		case reflect.Struct:
+			if err := checkStruct(path, vfield, input); err != nil {
+				return err
+			}
+		case reflect.Ptr:
+			if err := checkStruct(path, vfield.Elem(), input); err != nil {
+				return err
+			}
+		default:
+			if err := setFieldValue(path, vfield, tfield, input); err != nil {
+				return err
+			}
+		}
+
+	}
+	return nil
+}
+
+func getValues(path string, vfield reflect.Value, tfield reflect.StructField, input map[string]interface{}) (value interface{}, err error) {
+
+	validTagName := tfield.Tag.Get("valid")
+	lable, msg := getLable(tfield)
+	fname := getFullName(path, lable, tfield.Name)
+	svalue := fmt.Sprint(vfield.Interface())
+	check := func() (interface{}, error) {
+		v, ok := input[tfield.Name]
+		if !ok {
+			v, err = readFromCli(fname, validTagName, lable, msg)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return v, nil
+	}
+	switch {
+	case strings.HasPrefix(svalue, vc.ByInstall) || strings.EqualFold(svalue, fmt.Sprint(vc.ByInstallI)):
+		return check()
+	case isRequire(vfield, validTagName) && (!vfield.IsValid() || vfield.IsZero()):
+		return check()
+	case vfield.IsValid() && !vfield.IsZero() && validate(svalue, validTagName, lable, msg) != nil:
+		return check()
+	}
+	return nil, nil
+
+}
+
+func setSliceValue(path string, vfield reflect.Value, tfield reflect.StructField, input map[string]interface{}) (err error) {
+
+	//处理多个数据值问题
+	var v interface{}
+	if vfield.Len() > 0 {
+		listValue := make([]string, 0, 1)
+		for i := 0; i < vfield.Len(); i++ {
+			listValue = append(listValue, fmt.Sprint(vfield.Index(i).Interface()))
+		}
+		v, err = getValues(path, reflect.ValueOf(strings.Join(listValue, "")), tfield, input)
+	} else {
+		v, err = getValues(path, vfield, tfield, input)
+	}
+
+	if err != nil || v == nil {
+		return err
+	}
+	if r, ok := v.([]interface{}); ok {
+		return types.SetSlice(r, vfield, tfield)
+	}
+	if r, ok := v.(string); ok {
+		slist := strings.Split(r, "|")
+		vi := make([]interface{}, 0, len(slist))
+		for _, l := range slist {
+			vi = append(vi, l)
+		}
+		return types.SetSlice(vi, vfield, tfield)
+	}
+	return types.SetSlice([]interface{}{v}, vfield, tfield)
+}
+
+func setFieldValue(path string, vfield reflect.Value, tfield reflect.StructField, input map[string]interface{}) (err error) {
+	v, err := getValues(path, vfield, tfield, input)
+	if err != nil || v == nil {
+		return err
+	}
+	if err := types.SetWithProperType(v, vfield, tfield); err != nil {
+		return err
+	}
+	return nil
+}
+
+func readFromCli(name string, tagName string, lable string, msg string) (string, error) {
+
+	//检查in参数，包括in则使用select,否则为input
+	ps := regexp.MustCompile(`^in\((.*)\)`).FindStringSubmatch(tagName)
+	if len(ps) == 0 {
+
+		//input输入项
+		prompt := promptui.Prompt{
+			Label:    name,
+			Validate: func(input string) error { return validate(input, tagName, lable, msg) },
+		}
+		result, err := prompt.Run()
+		return result, err
+	}
+
+	//select选择项
+	prompt := promptui.Select{
+		Label: name,
+		Items: strings.Split(ps[1:][0], "|"),
+	}
+
+	_, result, err := prompt.Run()
+	return result, err
+
+}
+
+func isRequire(input reflect.Value, tagName string) bool {
+	return strings.Contains(tagName, "required")
+}
+
+//validate 验证值是否合法
+func validate(input string, tagName string, lable string, msg string) error {
+	if tagName == "" || tagName == "-" {
+		return nil
+	}
+	if len(input) < 1 {
+		return errors.New("至少包含1个字符")
+	}
+	in := map[string]interface{}{"name": input}
+	ck := map[string]interface{}{"name": tagName}
+	if ok, err := govalidator.ValidateMap(in, ck); !ok {
+		return fmt.Errorf("%s (%w)", types.GetString(msg, fmt.Sprintf("请输入正确的%s", lable)), err)
+	}
+	return nil
+}
+
+func getFullName(path string, name string, tname string) string {
+	return fmt.Sprintf("%s(%s)", name, strings.Join([]string{tname, path}, ","))
+}
+
+func getLable(tfield reflect.StructField) (string, string) {
+	tagName := tfield.Tag.Get("lable")
+	if tagName == "" || tagName == "-" {
+		return tfield.Name, ""
+	}
+	list := strings.Split(tagName, "|")
+	if len(list) > 1 {
+		return list[0], list[1]
+	}
+	return list[0], ""
+}
