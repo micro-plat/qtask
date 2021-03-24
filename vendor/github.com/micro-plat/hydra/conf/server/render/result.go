90d560870241df0696246a2f322ddb04ebaf26cf
zkfy <lib4net@163.com>
2021-03-24 16:56:20 +0800

    更新

--- vendor/github.com/micro-plat/hydra/conf/server/render/result.go
+++
@@ -1,8 +0,0 @@
-package render
-
-//Result render输出结果
-type Result struct {
-	Status      int
-	ContentType string
-	Content     string
-}
--- vendor/github.com/micro-plat/hydra/hydra/servers/pkg/middleware/tag.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-//Tag 添加服务标签
-func Tag() Handler {
-	return func(ctx IMiddleContext) {
-		ctx.Next()
-		ctx.Response().AddSpecial(ctx.APPConf().GetServerConf().GetServerType())
-	}
-}
